2e03104211bc4fb38ec0194b39d0abb8ad594213
Va2828Cz <valentin@cze-kalla.de>
2019-08-27 18:22:48 +0200

    3 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -9,23 +9,23 @@ export class FermatTestComponent implements OnInit {
 
   versuchskaninchen: number = 13;
   anzahl: number = 15;
-  ergebnis = "Die Zahl";
+  ergebnis = "Die Zahl ";
 
   constructor() { }
 
   fermattest() {
     let zeuge = 2;
     //Berechne zeuge^(versuchskaninchen-1) moidulo Versuchskaninchen
-    let zwischenergebnis =1;
-    for(let i = 0; i < this.versuchskaninchen-1; i++){
+    let zwischenergebnis = 1;
+    for (let i = 0; i < this.versuchskaninchen - 1; i++) {
 
       zwischenergebnis = zwischenergebnis * zeuge;
     }
-    if(zwischenergebnis === 1) {
-      this.ergebnis += zeuge + 'ist wahrscheinlich eine Primzahl';
+    if (zwischenergebnis === 1) {
+      this.ergebnis += this.versuchskaninchen + ' ist wahrscheinlich eine Primzahl';
 
-    }else{
-      this.ergebnis += zeuge + 'ist wahrscheinlich keine Primzahl';
+    } else {
+      this.ergebnis += this.versuchskaninchen + ' ist wahrscheinlich keine Primzahl';
 
     }
 
